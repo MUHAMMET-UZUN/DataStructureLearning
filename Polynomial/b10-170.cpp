6b884b5e6e792866a14d8a6603f182c9d6bebfc4
UMAMI <mhmmtuzn61@hotmail.com>
2023-06-24 20:50:32 +0300

    polynomial

--- Polynomial/b10-170.cpp
+++ Polynomial/b10-170.cpp
@@ -18,7 +18,7 @@ void Create(struct Poly *p)
     int i;
 
     printf("Number of terms: ");
-    scanf("%d", p->n);
+    scanf("%d", &p->n);
 
     p->terms = (struct Term *)malloc(p->n * sizeof(struct Term));
 
@@ -36,10 +36,50 @@ void Display(struct Poly p)
     printf("\n");
 }
 
+struct Poly *Add(struct Poly *p1, struct Poly *p2)
+{
+    int i, j, k;
+    struct Poly *sum;
+
+    sum = (struct Poly *)malloc(sizeof(struct Poly));
+    sum->terms = (struct Term *)malloc((p1->n + p2->n) * sizeof(struct Term));
+    i = j = k = 0;
+
+    while (i < p1->n && j < p2->n)
+    {
+        if (p1->terms[i].exp > p2->terms[j].exp)
+            sum->terms[k++] = p1->terms[i++];
+        else if (p1->terms[i].exp < p2->terms[j].exp)
+            sum->terms[k++] = p2->terms[j++];
+        else
+        {
+            sum->terms[k].exp = p1->terms[i].exp;
+            sum->terms[k++].coeff = p1->terms[i++].coeff + p2->terms[j++].coeff;
+        }
+    }
+
+    for (; i < p1->n; i++)
+        sum->terms[k++] = p1->terms[i];
+    for (; j < p2->n; j++)
+        sum->terms[k++] = p2->terms[j];
+
+    sum->n = k;
+    return sum;
+}
+
 int main()
 {
-    struct Poly p1;
+    struct Poly p1, p2, *p3;
     Create(&p1);
+    Create(&p2);
+    p3 = Add(&p1, &p2);
+
+    printf("\n");
     Display(p1);
+    printf("\n");
+    Display(p2);
+    printf("\n");
+    Display(*p3);
+
     return 0;
 }
